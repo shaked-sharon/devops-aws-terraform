1432ccdec273d2db687e767b833d826c47d3dec3
shaked-sharon <sharon.shaked@icloud.com>
2025-11-06 13:24:18 +0200

    remove old terraform/aws directory to use  clean structure for new part I project

--- terraform/aws/dev/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "EC2 Instance ID"
-  value       = module.ec2.instance_id
-}
-
-output "public_ip" {
-  description = "EC2 Public IP"
-  value       = module.ec2.public_ip
-}
--- terraform/aws/modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "EC2 Instance ID"
-  value       = aws_instance.this.id
-}
-
-output "public_ip" {
-  description = "Public IP address"
-  value       = aws_instance.this.public_ip
-}
